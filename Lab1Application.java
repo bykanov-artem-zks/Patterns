e9a4d1914714b5bf95123623a965f297c567ec3a
bykanov-artem-zks <bykanov2021zks11@student.karazin.ua>
2024-11-22 01:15:20 +0200

    Update Lab1Application.java

--- Lab1Application.java
+++ Lab1Application.java
@@ -15,5 +15,6 @@ public class Lab1Application implements CommandLineRunner{
 		Discriminant Discriminant = new Discriminant();
 		System.out.printf("D = %d^2 - 4*%d*%d = %d", Discriminant.getB(), Discriminant.getA(),
 				Discriminant.getC() ,Discriminant.getResult());
+		if(Discriminant.getResult()<0) System.out.printf("\nD<0");
 	}
 }
